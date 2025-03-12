6d5b2a25cd73901525e4ddb761117c1482f08f04
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-03-12 22:03:17 +0530

    a movie watchlist creator

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = process.env.PORT
+require('dotenv').config()
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
