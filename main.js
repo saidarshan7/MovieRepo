bf37c2c447e2f0509ccad8c93d9b04052647ffbb
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-03-20 22:59:18 +0530

    simple interest calculator backend logic and connecting to frontend

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Very Simple Task manger
+// //Very Simple Task manger
 
 
 const express = require('express')
@@ -6,65 +6,151 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
 
-const person = [];
+// const person = [];
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
-  res.send('Hello World! Welcome to <b>TASK MANGAER</b>')
+//   res.send('Hello World! Welcome to <b>TASK MANGAER</b>')
 
-})
+// })
 
 
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get('/tasks', (req,res)=>{
+// app.get('/tasks', (req,res)=>{
   
-    res.send(person)
-})
+//     res.send(person)
+// })
 
 
 
 
-app.post('/tasks', (req, res) => {
+// app.post('/tasks', (req, res) => {
 
     
 
-    console.log(req.body);
-    let i = person.length
+//     console.log(req.body);
+//     let i = person.length
    
-    person.push(
-        {   
-            id : i,
-            task : req.body.name
-        }
-    )
+//     person.push(
+//         {   
+//             id : i,
+//             task : req.body.name
+//         }
+//     )
    
-    console.log(i)
+//     console.log(i)
 
-    res.send('Got a POST request')
-  })
+//     res.send('Got a POST request')
+//   })
 
 
 
-  app.use(bodyParser.urlencoded({ extended: true }));
+//   app.use(bodyParser.urlencoded({ extended: true }));
 
      
 
-  app.delete('/tasks/:id', (req, res) => {
+//   app.delete('/tasks/:id', (req, res) => {
     
-     const index = person.indexOf(person[req.params.id]);
-     if (index > -1) { // only splice array when item is found
-     person.splice(index, 1); // 2nd parameter means remove one item only
-}
+//      const index = person.indexOf(person[req.params.id]);
+//      if (index > -1) { // only splice array when item is found
+//      person.splice(index, 1); // 2nd parameter means remove one item only
+// }
   
-    res.send("Removed Your Task at"+req.params.id + "place your new task list" );
+//     res.send("Removed Your Task at"+req.params.id + "place your new task list" );
     
 
+//   })
+
+
+
+
+
+
+// var users = [
+//     {
+//       name: "John",
+//       pets: [
+//         { type: "Dog", age: 3 },
+//         { type: "Cat", age: 2 }
+//       ]
+//     },
+//     {
+//       name: "Alice",
+//       pets: [
+//         { type: "Rabbit", age: 1 },
+//         { type: "Parrot", age: 4 }
+//       ]
+//     },
+//     {
+//       name: "Bob",
+//       pets: [
+//         { type: "Fish", age: 1 },
+//         { type: "Turtle", age: 5 }
+//       ]
+//     },
+//     {
+//       name: "Eve",
+//       pets: [
+//         { type: "Hamster", age: 2 },
+//         { type: "Dog", age: 6 }
+//       ]
+//     },
+//     {
+//       name: "Charlie",
+//       pets: [
+//         { type: "Cat", age: 3 },
+//         { type: "Rabbit", age: 2 }
+//       ]
+//     }
+//   ];
+
+  const cors = require("cors");
+  app.use(cors({
+    origin: "http://127.0.0.1:5500", // Restrict to this origin
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+  }));
+  
+  
+  
+  
+  
+  app.get('/',(req,res)=> {
+     const a = parseInt(req.query.a)
+     const b = parseInt(req.query.b)
+     const sum = a+ b;
+     res.json({
+      md:sum
+     })
+
   })
 
+  app.get('/SIC',(req,res)=>{
+    const p =  parseInt(req.query.p);
+    const r = parseInt(req.query.r);
+    const t = parseInt(req.query.t);
 
+    const SI = (p*r*t)/100;
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    res.json({
+      d : SI
+    })
   })
+
+  app.get('/edit',(req,res)=>{
+   users.push(
+      
+
+   )
+
+   console.log( req.query )
+   res.send("<h1>ok<h1>")
+  })
+  
+
+
+
+
+app.listen(3000);
